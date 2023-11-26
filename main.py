600c6c81cc3ac33c8cba6b2f1fc82248a6b123da
yamamoto-yuta <automatuX78@gmail.com>
2023-11-27 00:55:31 +0900

    コードを Slack Bolt 用に変更＆ ChatGPT API へ接続

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# main.py
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+import os
 
-app = FastAPI()
+from slack_bolt import App
+from openai import OpenAI
+
+app = App(
+    signing_secret=os.environ["SLACK_SIGNING_SECRET"],
+    token=os.environ["SLACK_BOT_TOKEN"]
+)
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 
-@app.post("/echo")
-def echo():
-    return JSONResponse(content={
-        "text": "echo"
-    })
+@app.command("/summarize")
+def summarize(ack, respond, command):
+    ack()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+    )
+
+    respond(completion.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    app.start(port=3000)
